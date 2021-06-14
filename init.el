73f37195dec8e11a0fca52cb3a4309f392c07f63
William Acevedo <william.acevedo@gmail.com>
2021-06-14 04:10:24 +0200

    Updates Emacs init file

--- init.el
+++ init.el
@@ -1,16 +1,17 @@
 ;; -*- mode: elisp -*-
 
-;; Disable the splash screen (to enable it again, replace the t with 0)
-(setq inhibit-startup-message t)
-
-;; Disable menus and set visible bell
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (setq visible-bell t)
-(setq display-line-numbers-mode t)
+(setq display-line-numbers-mode 1)
+;; Change to your .emacs.d/ directory accordingly
 (setq user-emacs-directory "/home/wacevedo/.emacs.d")
+
+;; Disable the splash screen (to enable it again, replace the t with 0)
+(setq inhibit-startup-message t)
+(setq package-enable-at-startup nil)
 
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/") t)
@@ -20,38 +21,37 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Change to your .emacs.d/ directory accordingly
+(use-package try
+  :ensure t)
 
-(require 'package)
-(require 'use-package)
-
-
-
-
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
 
 ;; Enable transient mark mode
 (transient-mark-mode 1)
-;;;;Org mode configuration
+
+;;Org mode configuration
 (use-package org
   :ensure t)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; Org mode key bindings
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
 
+;; Theme
 (use-package zenburn-theme
   :ensure t
   :config
   (load-theme 'zenburn t))
 
-
-;; Org mode key bindings
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme use-package notes-mode evil)))
+ '(package-selected-packages '(try zenburn-theme use-package notes-mode evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
