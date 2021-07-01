0aa9df55fe37a3d3452858857dec4eb56bb44bac
William Acevedo <william.acevedo@gmail.com>
2021-07-01 23:26:54 +0200

    Updates init.el to utilize org mode functionality to the Emacs config file and moves files to dedicated folder

---
+++ Emacs/init.el
@@ -0,0 +1,18 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" default))
+ '(package-selected-packages '(linum-relative evil-collection evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
