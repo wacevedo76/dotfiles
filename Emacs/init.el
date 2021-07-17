8bd99f6a2416531d98f416113263480d0d23ff8d
William Acevedo <william.acevedo@gmail.com>
2021-07-18 00:31:11 +0200

    Updates Emacs config files

--- Emacs/init.el
+++ Emacs/init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" default))
- '(package-selected-packages '(linum-relative evil-collection evil use-package)))
+ '(package-selected-packages '(dash nov linum-relative evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
